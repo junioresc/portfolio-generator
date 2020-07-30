895caea869a1175341c40b256b427eb65e4cc64b
Junioresc <junioresc1092@gmail.com>
2020-07-30 11:45:45 -0400

    messing around with the inquirer package

--- app.js
+++ app.js
@@ -9,7 +9,9 @@
 
 printProfileData(profileDataArgs); */
 
-const fs = require("fs");
+const inquirer = require("inquirer");
+
+/* const fs = require("fs");
 const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2);
 
@@ -19,4 +21,14 @@ fs.writeFile("./index.html", generatePage(name, github), err => {
     if (err) throw new Error(err);
 
     console.log("Portfolio complete! Check out index.html to see the output!");
-});
+}); */
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
