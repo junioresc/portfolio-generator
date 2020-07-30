6976260b32cde604c6a32adc9c62b1007849e3be
Junioresc <junioresc1092@gmail.com>
2020-07-30 12:32:00 -0400

    added the inquirer questions as stated in the issues tab and user is able to add multiple projects

--- app.js
+++ app.js
@@ -9,7 +9,8 @@
 
 printProfileData(profileDataArgs); */
 
-const inquirer = require("inquirer");
+const inquirer = require(`inquirer`);
+const { prompt } = require("inquirer");
 
 /* const fs = require("fs");
 const generatePage = require("./src/page-template.js");
@@ -23,12 +24,84 @@ fs.writeFile("./index.html", generatePage(name, github), err => {
     console.log("Portfolio complete! Check out index.html to see the output!");
 }); */
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    
+    return inquirer.prompt([
         {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
+            type: `input`,
+            name: `name`,
+            message: `What is your name?`
+        },
+        {
+            type: `input`,
+            name: `github`,
+            message: `Enter your GitHub Username`
+        },
+        {
+            type: `input`,
+            name: `about`,
+            message: `Provide some information about yourself:`
+        }
+    ]); 
+};
+
+const promptProject = portfolioData => {
+    // If there's no `projects` array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: `input`,
+            name: `name`,
+            message: `What is the name of your project?`
+        },
+        {
+            type: `input`,
+            name: `description`,
+            message: `Provide a description of the project (Required)`
+        },
+        {
+            type: `checkbox`,
+            name: `languages`,
+            message: `What language did you use with the project? (Check all that apply)`,
+            choices: [`JavaScript`, `HTML`, `CSS`, `ES6`, `jQuery`, `Bootstrap`, `Node`]
+        },
+        {
+            type: `input`,
+            name: `link`,
+            message: `Enter the GitHub link to your project. (Required)`
+        },
+        {
+            type: `confirm`,
+            name: `feature`,
+            message: `Would you like to feature this project?`,
+            default: false
+        },
+        {
+            type: `confirm`,
+            name: `confirmAddProject`,
+            message: `Would you like to enter another project?`,
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
